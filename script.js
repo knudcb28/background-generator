697456ec36dfe53fa809550a36e6341868593bd4
Chris Knudsvig <knudcb28@gmail.com>
2022-11-08 08:22:45 -0600

    adding starting project

---
+++ script.js
@@ -0,0 +1,45 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+const form = document.getElementById("form");
+const extra = document.getElementById("extra");
+const btn = document.getElementById("btn");
+
+
+
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+	btn.style.background = "linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+setGradient();
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+// button to generate random colors
+
+btn.addEventListener("click", surpriseMe)
+
+function surpriseMe() {
+	color1.value = randomColor()
+	color2.value = randomColor()
+
+	setGradient()
+}
+
+setGradient()
